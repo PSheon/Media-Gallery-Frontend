9febc85ca8516215c58f981dd321951e0d617b94
PSheon <pauljiang61020@gmail.com>
2022-11-22 10:03:45 +0800

    feat: updated seo tags.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,8 +21,13 @@ class CustomDocument extends Document {
             rel='stylesheet'
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
           />
-          <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-          <link rel='shortcut icon' href='/images/favicon.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#fa3b00' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#f44200'></meta>
         </Head>
         <body>
           <Main />
