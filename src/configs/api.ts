1e5bf22ec6b55d2f3c81dc1d776b18a055ec3bcb
PSheon <pauljiang61020@gmail.com>
2022-12-08 20:40:06 +0800

    feat: updated api config.

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,4 +1,4 @@
 export default {
-  baseUrl: process.env.NODE_ENV === 'production' ? 'https://mint.media.app' : 'http://localhost:1337',
-  publicFolderUrl: process.env.NODE_ENV === 'production' ? 'https://mint.media.app' : 'http://localhost:1337'
+  baseUrl: process.env.NODE_ENV === 'production' ? 'https://stage.media.app' : 'http://localhost:1337',
+  publicFolderUrl: process.env.NODE_ENV === 'production' ? 'https://stage.media.app' : 'http://localhost:1337'
 }
