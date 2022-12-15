610a3d911edc988a7220426247b9be087f800e0e
PSheon <pauljiang61020@gmail.com>
2022-12-15 18:44:54 +0800

    feat: hided view verse collaborator list.

--- src/views/verse/components/view/settingPanel/rightSection/index.tsx
+++ src/views/verse/components/view/settingPanel/rightSection/index.tsx
@@ -1,30 +1,32 @@
 // ** MUI Imports
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { Theme } from '@mui/material/styles'
+// import useMediaQuery from '@mui/material/useMediaQuery'
+// import { Theme } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
-import Divider from '@mui/material/Divider'
+
+// import Divider from '@mui/material/Divider'
 
 // ** Components Imports
 import ControlHintBox from 'src/views/verse/components/view/settingPanel/rightSection/ControlHintBox'
 import SettingBox from 'src/views/verse/components/view/settingPanel/rightSection/settingBox'
-import CollaboratorListMenu from 'src/views/verse/components/view/settingPanel/rightSection/CollaboratorsGroup'
+
+// import CollaboratorListMenu from 'src/views/verse/components/view/settingPanel/rightSection/CollaboratorsGroup'
 
 const RightSection = () => {
   // ** Hooks
-  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
+  // const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
 
   return (
     <Grid container spacing={4} justifyContent='flex-end' alignItems='center' sx={{ p: 4 }}>
-      {isDesktop && (
+      {/* {isDesktop && (
         <Grid item>
           <CollaboratorListMenu />
         </Grid>
-      )}
-      {isDesktop && (
+      )} */}
+      {/* {isDesktop && (
         <Grid item>
           <Divider orientation='vertical' sx={{ height: '2rem' }} />
         </Grid>
-      )}
+      )} */}
       <Grid item>
         <ControlHintBox />
       </Grid>
